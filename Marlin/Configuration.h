e28d928134899783afc4f39122a119364afdae6c
petrzmax <petrzmax@gmail.com>
2020-01-30 21:29:52 +0100

    Hemera calibrated & XY steps changed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -749,7 +749,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.3, 80.3, 1600, 409 } // XYZ - from stock firmware, E - Calibrated stock extruder - 402 (I'm using Hemera so now it's 409)
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 1600, 392.40 } // Calibrated stock extruder - 402 (Hemera stock 409, 392.40 - after calibration)
 
 /**
  * Default Max Feed Rate (mm/s)
